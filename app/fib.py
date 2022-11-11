8abf204c66118a9865e395971e163b0fd943f0a9
Nathan <naoswell@DESKTOP-CJUPV3Q.localdomain>
2022-11-11 14:02:05 -0500

    fixed potential merge conflict and edited main.yml job

--- app/fib.py
+++ app/fib.py
@@ -4,8 +4,6 @@ When given a position, the function returns the fibonacci at that position in th
 The zeroth number in the fibonacci sequence is 0. The first number is 1
 Negative numbers should return None
 """
-
-
 def fibonacci(position):
     if position < 0:
         raise ValueError("Invalid input")
